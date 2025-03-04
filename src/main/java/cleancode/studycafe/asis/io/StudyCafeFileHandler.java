318103e0ba28115194c120645a854112a02adcc6
seeeeeeong <lsinsung@gmail.com>
2025-03-04 19:48:02 +0900

    리팩토링(2) - 일급 컬렉션 적용

--- src/main/java/cleancode/studycafe/asis/io/StudyCafeFileHandler.java
+++ src/main/java/cleancode/studycafe/asis/io/StudyCafeFileHandler.java
@@ -1,8 +1,10 @@
 package cleancode.studycafe.asis.io;
 
 import cleancode.studycafe.asis.model.StudyCafeLockerPass;
+import cleancode.studycafe.asis.model.StudyCafeLockerPasses;
 import cleancode.studycafe.asis.model.StudyCafePass;
 import cleancode.studycafe.asis.model.StudyCafePassType;
+import cleancode.studycafe.asis.model.StudyCafePasses;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,7 +13,7 @@ import java.util.List;
 
 public class StudyCafeFileHandler {
 
-    public List<StudyCafePass> readStudyCafePasses() {
+    public StudyCafePasses readStudyCafePasses() {
         try {
             List<String> lines = Files.readAllLines(Paths.get("src/main/resources/cleancode/studycafe/pass-list.csv"));
             List<StudyCafePass> studyCafePasses = new ArrayList<>();
@@ -26,13 +28,13 @@ public class StudyCafeFileHandler {
                 studyCafePasses.add(studyCafePass);
             }
 
-            return studyCafePasses;
+            return StudyCafePasses.of(studyCafePasses);
         } catch (IOException e) {
             throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
         }
     }
 
-    public List<StudyCafeLockerPass> readLockerPasses() {
+    public StudyCafeLockerPasses readLockerPasses() {
         try {
             List<String> lines = Files.readAllLines(Paths.get("src/main/resources/cleancode/studycafe/locker.csv"));
             List<StudyCafeLockerPass> lockerPasses = new ArrayList<>();
@@ -46,7 +48,7 @@ public class StudyCafeFileHandler {
                 lockerPasses.add(lockerPass);
             }
 
-            return lockerPasses;
+            return StudyCafeLockerPasses.of(lockerPasses);
         } catch (IOException e) {
             throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
         }
