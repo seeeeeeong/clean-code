f75de8d54eb64c751b859d9efd1d436053c562f9
seeeeeeong <lsinsung@gmail.com>
2025-03-04 19:37:10 +0900

    리팩토링(2) - IO 통합

---
+++ src/main/java/cleancode/studycafe/asis/io/StudyCafeIOHandler.java
@@ -0,0 +1,47 @@
+package cleancode.studycafe.asis.io;
+
+import cleancode.studycafe.asis.model.StudyCafeLockerPass;
+import cleancode.studycafe.asis.model.StudyCafePass;
+import cleancode.studycafe.asis.model.StudyCafePassType;
+import java.util.List;
+
+public class StudyCafeIOHandler {
+
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputHandler outputHandler = new OutputHandler();
+
+    public void showWelcomeMessage() {
+        outputHandler.showWelcomeMessage();
+    }
+
+    public void showAnnouncement() {
+        outputHandler.showAnnouncement();
+    }
+
+    public void showPassOrderSummary(StudyCafePass selectedPass) {
+        outputHandler.showPassOrderSummary(selectedPass);
+    }
+
+    public void showPassOrderSummary(StudyCafePass selectedPass, StudyCafeLockerPass lockerPass) {
+        outputHandler.showPassOrderSummary(selectedPass, lockerPass);
+    }
+
+    public void showSimpleMessage(String message) {
+        outputHandler.showSimpleMessage(message);
+    }
+
+    public StudyCafePassType askPassTypeSelecting() {
+        outputHandler.askPassTypeSelection();
+        return inputHandler.getPassTypeSelectingUserAction();
+    }
+
+    public StudyCafePass askPassSelecting(List<StudyCafePass> passCandidates) {
+        outputHandler.showPassListForSelection(passCandidates);
+        return inputHandler.getSelectPass(passCandidates);
+    }
+
+    public boolean askLockerPass(StudyCafeLockerPass lockerPassCandidate) {
+        outputHandler.askLockerPass(lockerPassCandidate);
+        return inputHandler.getLockerSelection();
+    }
+}
